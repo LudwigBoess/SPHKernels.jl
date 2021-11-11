0aa417d5c5e6369e10bb267a6d38842ada8e271b
LudwigBoess <lboess@usm.lmu.de>
2021-11-11 17:23:18 +0100

    added C8 1D

--- src/wendland/C8.jl
+++ src/wendland/C8.jl
@@ -3,6 +3,11 @@ struct WendlandC8{T} <: AbstractSPHKernel
     norm::T
 end
 
+struct WendlandC8_1D{T} <: AbstractSPHKernel
+    dim::Int64
+    norm::T
+end
+
 """
     WendlandC8(T::DataType=Float64, n_neighbours::Integer=395)
 
@@ -10,7 +15,9 @@ Set up a `WendlandC8` kernel for a given DataType `T`.
 """
 function WendlandC8(T::DataType=Float64, dim::Integer=3)
 
-    if dim == 2
+    if dim == 1
+        return WendlandC8_1D{T}(1, 35/86)
+    elseif dim == 2
         norm = T(8/3π)
     elseif dim == 3
         norm = T(357/64π)
@@ -28,6 +35,46 @@ end
 Define `WendlandC8` kernel with dimension `dim` for the native `DataType` of the OS.
 """
 WendlandC8(dim::Integer) = WendlandC8(typeof(1.0), dim)
+
+
+"""
+    kernel_value(kernel::WendlandC8_1D{T}, u::Real, h_inv::Real) where T
+
+Evaluate WendlandC8 spline at position ``u = \\frac{x}{h}``.
+"""
+function kernel_value(kernel::WendlandC8_1D{T}, u::Real, h_inv::Real) where T
+
+    if u < 1
+        n = kernel.norm * h_inv
+        t1 = 1 - u
+        t8 = t1*t1*t1*t1*t1*t1*t1*t1
+        u2 = u*u
+        return ( t8 * t1 * (384u2 * u2 + 43u2*u + 2237u2 + 63u + 7 )) * n |> T
+    else
+        return 0 |> T
+    end
+
+end
+
+"""
+    kernel_deriv(kernel::WendlandC8_1D{T}, u::Real, h_inv::Real) where T
+
+Evaluate the derivative of the WendlandC8 spline at position ``u = \\frac{x}{h}``.
+"""
+function kernel_deriv(kernel::WendlandC8_1D{T}, u::Real, h_inv::Real) where T
+
+     if u < 1
+        n = kernel.norm * h_inv^2
+        t1 = 1 - u
+        t8 = t1*t1*t1*t1*t1*t1*t1*t1
+        u2 = u*u
+        return ( -156t8 * u * (32u2 * u + 25u2 + 8u + 1 )) * n |> T
+    else
+        return 0 |> T
+    end
+
+end
+
 
 """
     kernel_value(kernel::WendlandC8{T}, u::Real, h_inv::Real) where T
@@ -72,7 +119,7 @@ end
 
 Corrects the density estimate for the kernel bias. See Dehnen&Aly 2012, eq. 18+19.
 """
-function bias_correction( kernel::WendlandC8{T}, 
+function bias_correction( kernel::Union{WendlandC8_1D{T}, WendlandC8{T}},
                           density::Real, m::Real, h_inv::Real, 
                           n_neighbours::Integer ) where T
     return density |> T
